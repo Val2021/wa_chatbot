bd38db92212fb9d8b5e5febdf022428b2de4b645
Val Araujo <val.spminas@gmail.com>
2024-10-31 11:01:10 -0300

    fixing buttons design

--- src/Home.py
+++ src/Home.py
@@ -17,11 +17,10 @@ def main():
     if 'user_id' not in st.session_state:
         st.markdown('<h1 class="title">Welcome to our space!</h1>', unsafe_allow_html=True)
         username = st.text_input("Username:")
-        st.markdown('<div class="login-button">', unsafe_allow_html=True)
         if st.button("Log In"):
             st.session_state['user_id'] = username
             st.session_state['session_id'] = st.session_state.get('session_id', 0) + 1
-            st.success(f"Welcome, {username}!")
+            st.success(f"Welcome, {username}.Click again and have fun!")
             logging.info(f"User {username} logged in with session ID {st.session_state['session_id']}")
             st.markdown('</div>', unsafe_allow_html=True)
         return
@@ -29,10 +28,10 @@ def main():
     user_id = st.session_state['user_id']
     chatbot = Chatbot(user_id)
 
-    # #Response tone selection
+    # # #Response tone selection
     st.markdown('<h1 class="title">How can I assist you today!</h1>', unsafe_allow_html=True)
     tone_choice = st.radio("Select response tone:", ["***Formal***", "***Informal***"], index=0 if chatbot.response_tone == "Informal" else 1)
-    if st.button("Save Tone Preference"):
+    if tone_choice != chatbot.response_tone:
         chatbot.set_response_tone(tone_choice)
         st.success(f"Tone preference '{tone_choice}' saved.")
         logging.info(f"User {user_id} set response tone to: {tone_choice}")
@@ -48,8 +47,9 @@ def main():
     if st.button("Log Out"):
         # Clear session state
         st.session_state.clear()
-        st.success("You have been logged out.")
+        st.success("Are you leaving? Click again and see you later!")
         logging.info(f"User {user_id} logged out.")
+        st.stop()
     st.markdown('</div>', unsafe_allow_html=True)
 
 def get_img_as_base64(file):
